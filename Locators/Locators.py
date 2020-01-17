edfe56cca610fcb42d080551752c5b96d571636c
ECabrera3094 <57124413+ECabrera3094@users.noreply.github.com>
2020-01-17 15:24:49 -0600

    Add files via upload

---
+++ Locators/Locators.py
@@ -0,0 +1,33 @@
+class MyLocators:
+
+    # Atributos de Clase - Son Compartidos por TODAS las Instancias de la Clase.
+    ID_Name = "firstName"
+    ID_LastName = "lastName"
+    ID_MiddleName = "middleName"
+    CSS_Dont_MiddleName = ".sc-VigVT>div>input"
+    XPath_Month = "//div[@id='root']/div/div/div[2]/div/div/div[2]/div[3]/div/div/form/div[5]/div/div/div/div"
+    XPath_Day = "//div[@id='root']/div/div/div[2]/div/div/div[2]/div[3]/div/div/form/div[5]/div/div[2]/div/div"
+    XPath_Year = "//div[@id='root']/div/div/div[2]/div/div/div[2]/div[3]/div/div/form/div[5]/div/div[3]/div/div"
+    XPath_Gender = "//div[@id='root']/div/div/div[2]/div/div/div[2]/div[3]/div/div/form/div[6]/div/div/div"
+    ID_SSN = "ssn"
+    ID_ZipCode = "zipcode"
+    ID_Email = "email"
+    ID_Phone = "phone"
+    XPath_DLS = "//div[@id='root']/div/div/div[2]/div/div/div[2]/div[3]/div/div/form/div[11]/div/div/div"
+    ID_DLN = "driversLicenseNumber"
+    CSS_Certify_Information = ".sc-fBuWsC:nth-child(7) input"
+    XPath_Continue = "//span[contains(.,'Continue')]"
+    # ----------
+    XPath_Summary_Rights = "//*[@id='root']/div/div/div[2]/div/div/div[2]/div[3]/div[2]/div/div[2]/div/input"
+    XPath_Next_Document = "//*[@id='root']/div/div/div[2]/div/div/div[2]/div[3]/div[2]/div/div[4]/div/button/div/div/span"
+    # ----------
+    XPath_Disclosure_Background = "//*[@id='root']/div/div/div[2]/div/div/div[2]/div[3]/div[3]/div/div[2]/div/input"
+    XPath_Next_Document_2 = "//*[@id='root']/div/div/div[2]/div/div/div[2]/div[3]/div[3]/div/div[4]/div/button/div/div/span"
+    # ----------
+    XPath_Agreement = "//*[@id='root']/div/div/div[2]/div/div/div[2]/div[3]/div[4]/div/div[2]/div[1]/div/input"
+    XPath_ReviewData = "//*[@id='root']/div/div/div[2]/div/div/div[2]/div[3]/div[4]/div/div[2]/div[3]/div/button/div/div/span"
+    # ----------
+    ID_Name_Confirm = "name"
+    XPath_Draw_Signature = "//*[@id='signature-pad']/div[1]/canvas"
+    XPath_Indicate_Agreement = "/html/body/div[@id='root']/div/div/div[2]/div[@class='sc-hzDkRC lpnCDu']/div/div[2]/div[3]/div[5]/div/div[4]/div[3]/input"
+    XPath_Submit = "//*[@id='root']/div/div/div[2]/div/div/div[2]/div[3]/div[5]/div/div[6]/div/button/div/div/span"
